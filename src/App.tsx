d47853492c014e25eefeb86f71ba2f276015aeb3
RoxanaCamara <roxana.camara07@gmail.com>
2025-09-05 23:25:33 -0300

    feat: maquetado

--- src/App.tsx
+++ src/App.tsx
@@ -14,28 +14,40 @@ function App() {
       title: "Play",
       current: 10,
     },
+    {
+      title: "Work",
+      current: 32,
+      previous: 36,
+    },
+    {
+      title: "Play",
+      current: 10,
+    },
   ];
   return (
     <>
-      <div className="h-screen flex flex-col lg:flex-row gap-8 bg-[#0E1323]">
-        <div className="p-8 min-w-[327px]  flex items-center gap-4 bg-[#5747EA] rounded-2xl mb-8">
-          <img
-            src={jeremy}
-            className="h-16 border-white border-3 rounded-full"
-            alt="user profile"
-          />
-          <div className="flex flex-col items-start">
-            <p className="text-[15px] text-white">Report for</p>
-            <h2 className="text-2xl text-white">Jeremy Robson</h2>
+      <div className="h-screen flex flex-col lg:flex-row gap-8 bg-[#0E1323] px-4 py-16">
+        <div className="flex flex-col relative ">
+          <div className="p-8 min-w-[327px] flex items-center gap-4 bg-[#5747EA] rounded-2xl relative top-[20px] ">
+            <img
+              src={jeremy}
+              className="h-16 border-white border-3 rounded-full"
+              alt="user profile"
+            />
+            <div className="flex flex-col items-start">
+              <p className="text-[15px] text-white">Report for</p>
+              <h2 className="text-2xl text-white">Jeremy Robson</h2>
+            </div>
+          </div>
+          <div className="">
+            <Menu />
           </div>
         </div>
-        <Menu />
-      </div>
-
-      <div>
-        {cards.map((e) => (
-          <Card key={e.title} />
-        ))}
+        <div>
+          {cards.map((e) => (
+            <Card key={e.title} />
+          ))}
+        </div>
       </div>
     </>
   );
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,13 +2,15 @@ import ellipsis from "../../assets/icon-ellipsis.svg";
 
 export const Card = () => {
   return (
-    <div className="p-8 min-w-[327px]  flex flex-col gap-4 bg-[#1C204B] rounded-2xl mb-8">
+    <div className="p-8 min-w-[327px] min-h-[160px] flex flex-col gap-4 bg-[#1C204B] rounded-2xl mb-8">
       <div className="flex justify-between items-center">
         <h3 className="text-[18px] text-white">Work</h3>
         <img src={ellipsis} className="h-1" alt="ellipsise" />
       </div>
-      <h2 className="text-[56px] text-white">32hrs</h2>
-      <p className="text-[15px] text-[#BBC0FF]">Last Week - 36hrs</p>
+      <div className="flex flex-row lg:flex-col justify-between lg:justify-start items-center lg:items-start">
+        <h2 className="lg:text-[56px] text-[32px] text-white">32hrs</h2>
+        <p className="text-[15px] text-[#BBC0FF]">Last Week - 36hrs</p>
+      </div>
     </div>
   );
 };
