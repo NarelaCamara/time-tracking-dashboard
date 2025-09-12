afd9ec32141cbe62c4f733bc128b8359b31170e6
RoxanaCamara <roxana.camara07@gmail.com>
2025-09-12 14:45:48 -0300

    refactor: correccion de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import jeremy from "../src/assets/image-jeremy.png";
 
 import { Card } from "./components/card/Card";
 import { Menu } from "./components/menu/Menu";
+import { Profile } from "./components/profile/Profile";
 
 function App() {
   const cards = [
@@ -28,24 +29,10 @@ function App() {
     <>
       <div className="h-screen flex flex-col lg:flex-row m-6 md:items-center lg:justify-center bg-[#0E1323] px-4 pt-16">
         <div className="flex flex-col relative md:w-[612px] lg:h-[520px] lg:w-[255px]">
-          <div className="p-8 min-w-[327px] flex lg:flex-col items-start gap-2 bg-[#5747EA] rounded-2xl relative top-[10px] lg:h-full lg:p-8">
-            <img
-              src={jeremy}
-              className="h-16 border-white border-3 rounded-full"
-              alt="user profile"
-            />
-            <div className="flex flex-col items-start">
-              <p className="text-[15px] text-white">Report for</p>
-              <h2 className="text-2xl lg:text-[40px] text-white">
-                Jeremy Robson
-              </h2>
-            </div>
-          </div>
-
+          <Profile image={jeremy} />
           <Menu />
         </div>
-
-        <div className="grid  md:grid-cols-3  grid-rows-0 gap-4 md:w-[612px] lg:w-auto lg:max-w-[829px] lg:relative lg:-right-20">
+        <div className="grid grid-cols-3 gap-4">
           {cards.map((e) => (
             <Card key={e.title} />
           ))}
---
+++ src/components/profile/Profile.tsx
@@ -0,0 +1,15 @@
+export const Profile = ({ image }: { image: string }) => {
+  return (
+    <div className="p-8 min-w-[227px] flex lg:flex-col items-start gap-2 bg-[#5747EA] rounded-2xl relative top-[10px] lg:h-full lg:p-8">
+      <img
+        src={image}
+        className="h-16 border-white border-3 rounded-full"
+        alt="user profile"
+      />
+      <div className="flex flex-col items-start">
+        <p className="text-[15px] text-white">Report for</p>
+        <h2 className="text-2xl lg:text-[40px] text-white">Jeremy Robson</h2>
+      </div>
+    </div>
+  );
+};
