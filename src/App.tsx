d7a41847bf3a3f9e595a871862819034a945588e
RoxanaCamara <roxana.camara07@gmail.com>
2025-09-13 01:50:54 -0300

    feat: correcion desktop design

--- src/App.tsx
+++ src/App.tsx
@@ -54,16 +54,18 @@ function App() {
   ];
   return (
     <>
-      <div className="bg-[#0E1323] m-6 grid grid-cols-1 lg:grid-cols-3 lg:grid-rows-1 lg:gap-4 min-w-[350px]">
-        <div className="">
-          <Profile image={jeremy} />
-          <Menu />
-        </div>
-        <div className="lg:col-span-2">
-          <div className="grid grid-rows-1 md:grid-cols-3 gap-4">
-            {cards.map((e) => (
-              <Card key={e.title} />
-            ))}
+      <div className="bg-[#0E1323] max-md:min-w-[320px] max-md:max-w-[450px] lg:max-w-[1200px] ">
+        <div className="grid grid-cols-1 lg:grid-cols-3 lg:grid-rows-1 lg:gap-4 w-full">
+          <div className="lg:h-[520px] lg:col-span-1 sticky top-0 z-10">
+            <Profile image={jeremy} />
+            <Menu />
+          </div>
+          <div className="lg:col-span-2">
+            <div className="grid grid-rows-1 md:grid-cols-3 gap-4">
+              {cards.map((e) => (
+                <Card key={e.title} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
